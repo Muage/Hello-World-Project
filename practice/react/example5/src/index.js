e1c33caf1b6d9326e093022131b1b9fd8c6bbdfa
Muage <past_exp@naver.com>
2023-01-04 22:59:34 +0900

    연습노트

--- practice/react/example5/src/index.js
+++ practice/react/example5/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ProfileCard from './composition/ProfileCard';
+import DarkOrLight from './context/DarkOrLight';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ProfileCard />
+    <DarkOrLight />
   </React.StrictMode>
 );
 
