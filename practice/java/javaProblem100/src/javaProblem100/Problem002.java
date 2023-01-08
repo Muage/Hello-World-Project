8a46d6afce600c15ebd606fdadf689eac1d1dbba
Muage <past_exp@naver.com>
2023-01-08 22:18:21 +0900

    연습노트

--- practice/java/javaProblem100/src/javaProblem100/Problem002.java
+++ practice/java/javaProblem100/src/javaProblem100/Problem002.java
@@ -1,9 +1,11 @@
 package javaProblem100;
 
 // public: 접근제한자, 접근제어자 ( 클래스나 메서드에 접근할 수 있는 범위를 지정 ) -> private, protected, public, default
-// void: 반환 타입
+// void: 반환타입, 반환할 값이 없는 경우
 
-// 클래스선언 ( 카멜케이스표기법 사용 -> HelloWorld )
+// 클래스선언: 카멜케이스표기법 사용 (-> HelloWorld)
+// main method -> main(): 기본 메서드, 자바 프로그램 실행 시 가장 먼저 실행됨, 길게 작성된 소스에서 프로그램의 시작점을 알려주는 용도 ( entry point )
+// parameters: 메서드(함수) 호출 시 하나 또는 둘 이상의 파라미터 값을 넣어서 호출, 파라미터 값을 저장할 변수(-> args) 명시
 
 // 변수
 public class Problem002 {	// class: 객체를 생성하는 틀, 프레임, 공장, 템플릿
---
+++ practice/java/javaProblem100/src/javaProblem100/Problem003.java
@@ -0,0 +1,10 @@
+package javaProblem100;
+
+// º¯¼ö
+public class Problem003 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
