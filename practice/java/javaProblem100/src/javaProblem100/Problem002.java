08fecf1d7244226591e782900e8f8b5fe0e0e2bd
Muage <past_exp@naver.com>
2023-01-08 22:05:06 +0900

    연습노트

---
+++ practice/java/javaProblem100/src/javaProblem100/Problem002.java
@@ -0,0 +1,15 @@
+package javaProblem100;
+
+// public: 접근제한자, 접근제어자 ( 클래스나 메서드에 접근할 수 있는 범위를 지정 ) -> private, protected, public, default
+// void: 반환 타입
+
+// 클래스선언 ( 카멜케이스표기법 사용 -> HelloWorld )
+
+// 변수
+public class Problem002 {	// class: 객체를 생성하는 틀, 프레임, 공장, 템플릿
+	
+	public static void main(String[] args) {	// method: 함수, 어떤 특정한 동작이나 작업, 행위 등을 수행하는 것
+		System.out.println("Hello World!");
+	}
+
+}
