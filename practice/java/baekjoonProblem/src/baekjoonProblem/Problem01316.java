64ee299dcb351504a9bdec3348e6e79a2d1f6036
Muage <past_exp@naver.com>
2023-01-18 23:32:18 +0900

    연습노트

--- practice/java/baekjoonProblem/src/baekjoonProblem/Problem01316.java
+++ practice/java/baekjoonProblem/src/baekjoonProblem/Problem01316.java
@@ -1,5 +1,7 @@
 package baekjoonProblem;
 
+import java.util.*;
+
 // 문제
 // 그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다.
 // 예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만, aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
@@ -44,5 +46,43 @@ package baekjoonProblem;
 // (5)	1
 
 public class Problem01316 {
+	
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		int count = 0;
+		int n = scanner.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			if(check() == true) {
+				count++;
+			}
+		}
+		
+		System.out.println(count);
+	}
+	
+	public static boolean check() {
+		int prev = 0;
+		boolean[] check = new boolean[26];
+		String word = scanner.next();
+		
+		for(int i = 0; i < word.length(); i++) {
+			int now = word.charAt(i);
+			
+			if(prev != now) {
+				if(check[now - 'a'] == false) {
+					check[now - 'a'] = true;
+					prev = now;
+				} else {
+					return false;
+				}
+			} else {
+				continue;
+			}
+		}
+		return true;
+	}
 
 }
