6e5950c100d6317dc30fd8bc90c5b96bf544dc46
Muage <past_exp@naver.com>
2023-01-23 01:17:21 +0900

    미니 로컬데이터

--- project/personal/mini/mini_localStorage/localStorage.js
+++ project/personal/mini/mini_localStorage/localStorage.js
@@ -53,7 +53,7 @@ window.onload = () => {
 
     /* localStorage Data All View  버튼 */
     const btnAllView = document.querySelector('.btnAllView');
-    console.log(btnAllView);
+    // console.log(btnAllView);
 
     /* localStorage Data All View  버튼 클릭 시 */
     btnAllView.addEventListener('click', () => {
@@ -62,38 +62,45 @@ window.onload = () => {
         // console.log(getData);
 
         /* 배열 정보를 동적으로 테이블 생성하여 삽입하기 */
-        let arr = new Array();
-        let result = "";
+        // let arr = new Array();
+        // let result = "";
 
-        arr.push({name: "홍길동", email: "hong@example.com", age: 25, pastime: "음악감상"});
-        arr.push({name: "이순신", email: "lee@example.com", age: 33, pastime: "산책"});
-        arr.push({name: "강감찬", email: "kang@example.com", age: 44, pastime: "영화감상"});
-        arr.push({name: "김유신", email: "kim@example.com", age: 55, pastime: "야식먹기"});
-        arr.push({name: "심청이", email: "shim@example.com", age: 66, pastime: "독서"});
+        // arr.push({name: "홍길동", email: "hong@example.com", age: 25, pastime: "음악감상"});
+        // arr.push({name: "이순신", email: "lee@example.com", age: 33, pastime: "산책"});
+        // arr.push({name: "강감찬", email: "kang@example.com", age: 44, pastime: "영화감상"});
+        // arr.push({name: "김유신", email: "kim@example.com", age: 55, pastime: "야식먹기"});
+        // arr.push({name: "심청이", email: "shim@example.com", age: 66, pastime: "독서"});
 
-        console.log(arr[0].name);
-        console.log(arr[0].email);
-        console.log(arr[0].age);
-        console.log(arr[0].pastime);
+        // console.log(arr[0].name);
+        // console.log(arr[0].email);
+        // console.log(arr[0].age);
+        // console.log(arr[0].pastime);
 
         /* 반복문 순회하면서 배열 정보 result 변수에 누적 */
-        for(const i in arr) {
+        // for(const i in arr) {
             // console.log(i);
-            result += "<tr>";
-            result += "<td>" + arr[i].name + "</td>";
-            result += "<td>" + arr[i].email + "</td>";
-            result += "<td>" + arr[i].age + "</td>";
-            result += "<td>" + arr[i].pastime + "</td>";
-            result += "</tr>";
-        }
+            // result += "<tr>";
+            // result += "<td>" + arr[i].name + "</td>";
+            // result += "<td>" + arr[i].email + "</td>";
+            // result += "<td>" + arr[i].age + "</td>";
+            // result += "<td>" + arr[i].pastime + "</td>";
+            // result += "</tr>";
+        // }
         // console.log(result);
 
         // let htmlTbody = document.getElementById('html-tbody');
         // console.log(htmlTbody);
 
         /* Append */
-        $('#html-tbody').empty();
-        $('#html-tbody').append(result);
+        // $('#html-tbody').empty();
+        // $('#html-tbody').append(result);
+
+        /* Append-JS */
+        // htmlTbody.append(result);       // 텍스트 문자열 형태로 append
+        // htmlTbody.appendChild(result);  // node err
+        // htmlTbody.innerText(result);    // 괄호가 x
+        // htmlTbody.innerText = result;
+        // htmlTbody.innerHTML = result;
 
         /* Key만 출력 */
         // console.log(localStorage.key(0));   // userId
@@ -104,6 +111,43 @@ window.onload = () => {
 
         /* Key 개수 */
         // console.log(localStorage.length);
+
+        /* 결과 변수 -> 반복문 쓰지 않고 하나만 출력 */
+        // let result = "";
+        // result += "<tr>";
+        // result += "<td class='align-middle' width='30%'>" + localStorage.key(2) + "</td>";
+        // result += "<td class='align-middle' width='30%'>" + localStorage.getItem(localStorage.key(2)) + "</td>";
+        // result += "<td><button class='btnRemove'>Remove</button></td>";
+        // result += "</tr>";
+        // console.log(result);
+
+        // let htmlTbody = document.getElementById('html-tbody');
+        // htmlTbody.innerHTML = result;
+
+        /* Key 순회하면서 key: value 값 출력하기 */
+        // console.log("로컬 스토리지 길이: " + localStorage.length);
+        let arr = new Array();
+        let result = "";
+
+        for(let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            // console.log(`${key}: ${localStorage.getItem(key)}`);
+
+            /* 결과 변수 */
+            result += "<tr>";
+            result += "<td class='align-middle' width='30%'>" + key + "</td>";
+            result += "<td class='align-middle'>" + localStorage.getItem(key) + "</td>";
+            result += "<td><button class='btnRemove'>Remove</button></td>";
+            result += "</tr>";
+
+            /* 배열에 저장 */
+            arr.push(result);
+        }
+        // console.log(arr);
+
+        /* append 붙이기 */
+        $('#html-tbody').empty();
+        $('#html-tbody').append(result);
     });
 
 }
