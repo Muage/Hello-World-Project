e62571c82dc2656a4327c0569e3fba161abead9a
Muage <past_exp@naver.com>
2023-01-26 20:15:09 +0900

    연습노트

---
+++ practice/java/baekjoonProblem/src/baekjoonProblem/Problem01978.java
@@ -0,0 +1,44 @@
+package baekjoonProblem;
+
+import java.util.*;
+
+// 문제 ( 기본 수학 2 단계 )
+// 주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫 줄에 수의 개수 N이 주어진다.
+// N은 100이하이다.
+// 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
+
+// 출력
+// 주어진 수들 중 소수의 개수를 출력한다.
+
+// 예제 입력
+// 4
+// 1 3 5 7
+
+// 예제 출력
+// 3
+
+public class Problem01978 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		int count = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int num = scanner.nextInt();
+			
+			for(int j = 2; j <= num; j++) {
+				if(num == j) count++;
+				if(num == 1 || num % j == 0) break;
+			}
+		}
+		System.out.println(count);
+		
+		scanner.close();
+	}
+
+}
