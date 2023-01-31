f8f1399f439e01720ba848d6a45d40d6e6f732f9
Muage <past_exp@naver.com>
2023-01-31 22:57:34 +0900

    연습노트

--- practice/java/baekjoonProblem/src/baekjoonProblem/Problem11650.java
+++ practice/java/baekjoonProblem/src/baekjoonProblem/Problem11650.java
@@ -1,5 +1,8 @@
 package baekjoonProblem;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 // 문제 ( 정렬 단계 )
 // 2차원 평면 위의 점 N개가 주어진다.
 // 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
@@ -28,5 +31,31 @@ package baekjoonProblem;
 // 3 4
 
 public class Problem11650 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		int[][] arr = new int[n][2];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i][0] = scanner.nextInt();
+			arr[i][1] = scanner.nextInt();
+		}
+		
+		Arrays.sort(arr, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		});
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+		
+		scanner.close();
+	}
 
 }
