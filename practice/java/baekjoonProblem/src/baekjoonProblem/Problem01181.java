83a4c9e33d3c4bd537ba9189800b1059e1510cb3
Muage <past_exp@naver.com>
2023-02-01 21:18:46 +0900

    연습노트

--- practice/java/baekjoonProblem/src/baekjoonProblem/Problem01181.java
+++ practice/java/baekjoonProblem/src/baekjoonProblem/Problem01181.java
@@ -1,5 +1,9 @@
 package baekjoonProblem;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 // 문제 ( 정렬 단계 )
 // 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
 // 1. 길이가 짧은 것부터
@@ -45,5 +49,36 @@ package baekjoonProblem;
 // hesitate
 
 public class Problem01181 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		String[] arr = new String[n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = scanner.next();
+		}
+		
+		Arrays.sort(arr, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				if(s1.length() == s2.length()) {
+					return s1.compareTo(s2);
+				} else {
+					return s1.length() - s2.length();
+				}
+			}
+		});
+		
+		System.out.println(arr[0]);
+		
+		for(int i = 1; i < n; i++) {
+			if(!arr[i].equals(arr[i - 1])) {
+				System.out.println(arr[i]);
+			}
+		}
+		
+		scanner.close();
+	}
 
 }
