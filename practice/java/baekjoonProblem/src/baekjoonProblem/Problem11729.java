2bfaf3c92f8af6e17745a281e9ddd89ad8e777b0
Muage <past_exp@naver.com>
2023-02-09 15:57:01 +0900

    연습노트

--- practice/java/baekjoonProblem/src/baekjoonProblem/Problem11729.java
+++ practice/java/baekjoonProblem/src/baekjoonProblem/Problem11729.java
@@ -1,5 +1,7 @@
 package baekjoonProblem;
 
+import java.util.Scanner;
+
 // 문제 ( 재귀 단계 )
 // 세 개의 장대가 있고 첫 번째 장대에는 반경이 서로 다른 n개의 원판이 쌓여 있다.
 // 각 원판은 반경이 큰 순서대로 쌓여있다.
@@ -32,5 +34,33 @@ package baekjoonProblem;
 // 1 3
 
 public class Problem11729 {
+	
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		
+		sb.append((int)(Math.pow(2, n) - 1)).append("\n");
+		
+		Hanoi(n, 1, 2, 3);
+		System.out.println(sb);
+		
+		scanner.close();
+	}
+	
+	public static void Hanoi(int n, int start, int mid, int to) {
+		if(n == 1) {
+			sb.append(start + " " + to + "\n");
+			return;
+		}
+		
+		Hanoi(n - 1, start, to, mid);
+		
+		sb.append(start + " " + to + "\n");
+		
+		Hanoi(n - 1, mid, start, to);
+	}
 
 }
