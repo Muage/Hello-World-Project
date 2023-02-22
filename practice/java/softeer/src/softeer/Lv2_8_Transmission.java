b0c61bb3b12125e7163771991bbee82e85d07415
Muage <past_exp@naver.com>
2023-02-22 15:31:35 +0900

    연습노트

--- practice/java/softeer/src/softeer/Lv2_8_Transmission.java
+++ practice/java/softeer/src/softeer/Lv2_8_Transmission.java
@@ -3,7 +3,6 @@ package softeer;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 // 문제 ( Lv2. 8단 변속기 )
