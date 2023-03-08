a968454e71f64794f031c590845ca823d6b5b05d
Muage <past_exp@naver.com>
2023-03-08 11:02:33 +0900

    연습노트

--- practice/java/softeer/src/softeer/Lv2_GBC.java
+++ practice/java/softeer/src/softeer/Lv2_GBC.java
@@ -2,6 +2,7 @@ package softeer;
 
 // 문제 ( Lv2. GBC )
 // 글로벌 비즈니스 센터(GBC, Global Business Center)는 현대자동차그룹 통합 사옥이다.
+// 지하 7층, 지상 105층, 높이 약 570m의 규모로 2026년 하반기에 완공을 목표로 현재 공사 중에 있다.
 
 public class Lv2_GBC {
 
