3093b528636ab2882a3e94e177bbc827c570cb6c
Muage <past_exp@naver.com>
2023-03-26 14:32:33 +0900

    연습노트

--- practice/database/sqld/mock exam/mock_exam_1.sql
+++ practice/database/sqld/mock exam/mock_exam_1.sql
@@ -19,3 +19,18 @@
 --    ¨ê min(COL1), max(COL2), max(COL1)
 --    
 --A. ¨é
+
+create table SQLD_37 (
+    COL1 int,
+    COL2 int,
+    COL3 int
+);
+
+select * from SQLD_37;
+
+--insert into SQLD_37 (COL1, COL2, COL3) values (null, 0, 30);
+insert into SQLD_37 values (null, 0, 30);
+insert into SQLD_37 values (0, null, 0);
+insert into SQLD_37 values (10, 20, null);
+insert into SQLD_37 values (11, 21, 31);
+insert into SQLD_37 values (12, 22, 32);
