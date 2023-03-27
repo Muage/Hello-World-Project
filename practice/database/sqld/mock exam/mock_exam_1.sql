b1a9fd25e65d9ab3bbda21e42225758b93a078fa
Muage <past_exp@naver.com>
2023-03-27 14:32:33 +0900

    연습노트

--- practice/database/sqld/mock exam/mock_exam_1.sql
+++ practice/database/sqld/mock exam/mock_exam_1.sql
@@ -54,3 +54,34 @@ select max(COL1) AS A, min(COL2) AS B, sum(COL1) AS C from SQLD_37;
 select min(COL1) AS A, max(COL2) AS B, max(COL1) AS C from SQLD_37;
 --A   B   C
 --0  22   12
+
+
+--Q2. 다음의 SQL문을 사용할 때, 날짜형 변수를 문자형으로 바꿀 수 있는 것은?
+--    SELECT SYSDATE FROM DUAL;
+--    
+--    ① TO_NUMBER
+--    ② TO_DATE
+--    ③ TO_CHAR
+--    ④ CONVERT
+
+--A. ③
+
+--①
+--TO_NUMBER : 주어진 문자열 또는 컬럼의 데이터를 숫자로 변환
+SELECT TO_NUMBER(SYSDATE, 'YYYY-MM-DD') FROM DUAL;
+--오류
+
+--②
+--TO_DATE : 문자를 날짜 데이터 타입으로 변환
+SELECT TO_DATE(SYSDATE, 'YYYY-MM-DD') FROM DUAL;
+--23/03/31
+
+--③
+--TO_CHAR : 날짜 및 숫자 등의 값을 문자열로 변환
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD') FROM DUAL;
+--2023-03-31
+
+--④
+--CONVERT : 문자세트를 다른 문자세트로 문자열을 변환
+SELECT CONVERT(SYSDATE, 'YYYY-MM-DD') FROM DUAL;
+--오류
